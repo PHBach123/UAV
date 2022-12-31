0dd496f4d5e4d534863f33241f83fbb0ddbe03de
Mr-Duo <103053799+Mr-Duo@users.noreply.github.com>
2022-12-31 11:08:25 +0700

    Update Untitled2.cpp

--- Untitled2.cpp
+++ Untitled2.cpp
@@ -4,48 +4,52 @@
 // Table 1, paper 1
 double const H = 10;
 double const Vmax = 20;            
-double const Noise = 0.000000001;
-double const S = 50000000;   
+double const Noise = pow(10,-6);
+double const S = 70;   
 double const Ps = 0.039810717055;              
-double const Pwpt = 1000;            
-double const w0 = 0.001;      
+double const Pwpt = pow(10,7);            
+double const w0 = pow(10,-3);      
 double const alpha = 2.3;         
-double const Pb = 0.000001;
-double const Pu = 0.008;     
+double const Pb = pow(10,-3);
+double const Pu = 5;     
 double const DelT = 0.5;
 double const muy = 0.84;
 double const Nmax = 0.5;
-double const E = pow(10, 1.6 * 6);
-double const B = 1000000; //mhz
+double const E = 0.5772156649;
+double const B = 20; //mhz
 double const I = 0.1;
+double const sigma = 0.5;
 
 // Table 1, paper 2
-double const W = 100;
+double const W = 0.5;
 double const p = 1.225;
-double const R = 0.5;
+double const R = 0.08;
 double const A = 0.79;
-double const Ohm = 400;
+double const Ohm = 100;
 double const s = 0.05;
-double const d0 = 0.3;
+double const d0 = 0.0151;
 double const k = 0.1;
 double const del = 0.012;
-double const v0 = 7.2;
+double v0 = sqrt(W / (2*p*A));
 
-double const ws[2] = {5,0};   //v? trí ngu?n
-double const wd[2] = {15,0};  //v? trí dích
-
+struct Loca {
+	double x;
+	double y;
+};
+Loca ws, wd;
 int const N = 10;  //s? khe th?i gian
 double Tn = 0.5;   // 
-double q[N*N];
+Loca q[N];
 
 double f22a = 0, f22b = 0, f22d1 = 0, f22d2 = 0;
 double F22a = 0, F22b = 0, F22d1 = 0, F22d2 = 0;
 double dsu, ddu;
 
 void Calc(double q1, double q2, double d) {
-	dsu = sqrt( H*H + (q1 - ws[0]) * (q1 - ws[0]) + (q2 - ws[1]) * (q2 - ws[1]));
-	ddu = sqrt( H*H + (q1 - wd[0]) * (q1 - wd[0]) + (q2 - wd[1]) * (q2 - wd[1]));
-	f22a = B * Tn * DelT / log(2) * log(1 + (exp(-E) * w0 / Noise) * (Nmax * w0 * Ps + Pu * ceil(sqrt(Noise)) * pow(dsu,alpha)) / pow(dsu,alpha) / pow(ddu,alpha) );
+	dsu = sqrt( H*H + (q1 - ws.x) * (q1 - ws.x) + (q2 - ws.y) * (q2 - ws.y));
+	ddu = sqrt( H*H + (q1 - wd.x) * (q1 - wd.x) + (q2 - wd.y) * (q2 - wd.y));
+
+	f22a = B * Tn * DelT / log(2) * log(1 + (exp(-E) * w0 / Noise) * (Nmax * w0 * Ps + Pu * ceil(sigma) * pow(dsu,alpha)) / pow(dsu,alpha) / pow(ddu,alpha) );
 	f22b = B * Tn * DelT / log(2) * log(1 + (exp(-E) * w0 * Ps) / pow(dsu,alpha) / Noise);
 	
 	// Ham nang luong
@@ -85,19 +89,22 @@ int Lcheck(int d) {
 
 int main () {
 	// init
-	// v? trí d?u
-		q[0] = 0;  q[N] = 20;
-	// v? trí cu?i
-		q[N-1] = 10; q[N*N - 1] = 10;
-	
-	Calc(q[0], q[N], 0);
+	q[0].x = 0; q[0].y = 10;
+	q[N-1].x = 20; q[N-1].y = 10;
+	ws.x = 5; ws.y = 0;
+	wd.x = 15; wd.y = 0;
+	Calc(q[0].x, q[0].y, 0);
 	F22a = f22a;
 	F22b = f22b;
 	F22d1 = f22d1;
 	F22d2 = f22d2;
 	
-	printf("%lf %lf %lf %lf",f22a, f22b, f22d1, f22d2);
+	printf("%lf %lf %lf %lf\n",f22a, f22b, f22d1, f22d2);
+	printf("%d %d %d %d",BScheck(), Scheck(), Echeck(), Lcheck(0));
 	
 	//
+	
+	
+	
 	
 }
